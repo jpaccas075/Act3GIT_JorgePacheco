33a34d96e72ce9f772196839a65538fa1746beec
Jorge Pacheco <a_jorge.pacheco.castro@iespablopicasso.es>
2023-11-22 10:10:21 +0100

    Modifico para añadir más números aleatorios (30)

--- Actividad3ED/src/NumerosAleatoriosApp.java
+++ Actividad3ED/src/NumerosAleatoriosApp.java
@@ -5,7 +5,7 @@ public class NumerosAleatoriosApp {
 	public static void main(String[] args) {
 		Random generador = new Random();
 
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 30; i++) {
 			double r = generador.nextDouble(1.0, 10.0);
 			System.out.print((int) r + "\t");
 		}
